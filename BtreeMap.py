e720d5de03e7bb666db6625e6368fa358b8b2424
redARA <roma.ablyazoff@yandex.ru>
2023-03-22 16:47:57 +0300

    add cleening tree

--- BtreeMap.py
+++ BtreeMap.py
@@ -2,9 +2,27 @@ from BTrees._OOBTree import OOBTree
 
 btrees = OOBTree()
 
+# добовляет пару и изменяет значение у ключа
 def AddDuo(key, value):
   btrees[key] = value
 
+# выводит пару
 def PrintTree():
-  for pair in btrees.iteritems(): 
-    print (pair)
+  if len(btrees) == 0:
+    print("Дерева нет!")
+  else: 
+    for pair in btrees.iteritems(): 
+      print (pair)
+
+# вывод значения из ключа
+def GetValue(key):
+  if key in btrees:
+    print(key, " => " ,btrees[key])
+  else:
+    print("нет такого ключа")
+
+# очищает дерево
+def DoVoidTree():
+  for i in list(btrees.keys()):
+    del btrees[i] 
+  print("Дерево стерто")
